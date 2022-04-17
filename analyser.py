4913b1a35e5678b93a68fd91e1e22efd4413bc98
Michel Matthe <matthe.michel@gmail.com>
2022-04-18 01:51:43 +0200

    refactored analyser.py to not use deprecated pandas.append

--- analyser.py
+++ analyser.py
@@ -1,3 +1,4 @@
+import pandas
 import pandas as pd
 import csv
 
@@ -23,27 +24,22 @@ def analyse(gpu_data_path, gpu_price_requirements):
     :param gpu_price_requirements: path of the file where the GPU model and price requirements are stored
     :return: Pandas DataFrame that contains gpu listings which fit the model and price requirements.
     """
-    df = pd.read_csv(gpu_data_path, sep=';', decimal=',', index_col=0)
-    df.price = pd.to_numeric(df.price.replace(r'\.', '', regex=True).replace(',', '.', regex=True))
+    df_gpus = pd.read_csv(gpu_data_path, sep=';', decimal=',', index_col=0)  # dataframe of all gpu listings
+    df_gpus.price = pd.to_numeric(df_gpus.price.replace(r'\.', '', regex=True).replace(',', '.', regex=True))
 
     with open(gpu_price_requirements) as f:
         gpu_prices = [tuple(line) for line in csv.reader(f)]
 
-    df_analysed = pd.DataFrame(data=None, columns=['title', 'price', 'link'])
+    df_gpu_deals = pd.DataFrame(data=None, columns=['title', 'price', 'link'])  # dataframe of all gpu listing that
+    # match the model and price criteria
 
     for gpu, price in gpu_prices:
         regex = make_regex(gpu)
-        gpu_deals = df['title'].str.contains(rf'(?i){regex}')
-        gpu_deals = df[gpu_deals]
-        gpu_deals = gpu_deals[gpu_deals['price'] <= float(price)]
-        df_analysed = df_analysed.append(gpu_deals[['title', 'price', 'link']], ignore_index=True)  # TODO replace with concat
+        gpu_model_deals = df_gpus['title'].str.contains(rf'(?i){regex}')  # (?i) -> case insensitive
+        gpu_model_deals = df_gpus[gpu_model_deals]  # all listings that match specific gpu model name
+        # all listings that match specific gpu model name AND are under the set max price
+        gpu_model_deals = gpu_model_deals[gpu_model_deals['price'] <= float(price)]
 
-        print("\n\n")
-        print(df_analysed)
+        df_gpu_deals = pd.concat([df_gpu_deals, gpu_model_deals[['title', 'price', 'link']]], ignore_index=True, axis=0)
 
-    print("\n\n######################################################\n")
-    print(df_analysed)
-    return df_analysed
-
-
-# print(analyse('data/mainTest.csv'))
+    return df_gpu_deals
